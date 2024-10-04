3b65a34b44d8134d004f70255195513b2953beef
= <=>
2024-10-04 09:43:48 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import re
 from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
     """ Returns the log message obfuscated """
     for f in fields:
         message = re.sub(f"{f}=.*?{separator}",
