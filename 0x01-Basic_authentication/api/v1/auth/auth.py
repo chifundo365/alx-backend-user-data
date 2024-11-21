e4a40a8362ba6b8d03667bc82e1e175dbf134f30
= <=>
2024-11-21 19:34:17 +0000

    Define which routes dont need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,11 +9,17 @@ from typing import List, TypeVar
 class Auth:
     """ Manage API Authentication
     """
-    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
-        """ check if a path require authentication
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ check if a path is not in the excluded path
         Return:
-            - True | False
+            - True or False
         """
+        if path[-1] != "/":
+            path = path + "/"
+        if path is not in excluded_paths or path is None:
+            return True
+        if excluded_paths is None or not len(excluded_paths):
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
