4380646b800c562e53c459a8ca85291cf9fa6a6b
= <=>
2024-11-23 22:38:57 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,7 +20,7 @@ if auth_type == "auth":
     auth = Auth()
 elif auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
