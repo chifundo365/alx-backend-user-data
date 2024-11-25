cb3ae8ba0449c675411bac46f787e6c4ef4cb74d
= <=>
2024-11-25 10:00:55 +0000

    Basic-Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic authentication """
 from .auth import Auth
 import re
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,15 @@ class BasicAuth(Auth):
         if re.search(r'^Basic', header):
             return header[6:]
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ Decodes a Base64 Authorization header """
+        header = base64_authorization_header
+        if not header or not isinstance(header, str):
+            return None
+        try:
+            return base64.b64decode(header).decode('uutf-8')
+        except Exception as e:
+            return None
