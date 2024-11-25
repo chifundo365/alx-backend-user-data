b6ed72624e6077c511ce1114ebc12750e5e07cc0
= <=>
2024-11-25 09:28:01 +0000

    Basic=Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """ Basic authentication """
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """ Basic Authentication class """
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        Extract the Base64 part of the Authorization header
+        """
+        header = authorization_header
+        if not header or not isinstance(header, str):
+            return None
+        if re.search(r'^Basic', header):
+            return header[5:]
+        return None
