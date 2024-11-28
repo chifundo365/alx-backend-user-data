36f5c5c7a7b0eaa2ce0090746b6faf1e3201280b
= <=>
2024-11-28 22:25:37 +0000

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """ Session Authentication """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Authentication class """
-    pass
+    user_id_by_session = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates Assesiob ID for a user_id """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        ss_id = uuid.uuid4()
+        SessionAuth.user_id_by_session[ss_id] = user_id
+
+        return ss_id
+
