b23393d8ba3fb85aec02defea952970baa651e3f
= <=>
2024-11-28 21:09:19 +0000

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -28,8 +28,8 @@ class Auth:
         for ex_path in excluded_paths:
             if ex_path[-1] == "*":
                 _ex_path = ex_path[0: -1] + ".*"
-            if re.search(_ex_path, path):
-                return False
+                if re.search(_ex_path, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
