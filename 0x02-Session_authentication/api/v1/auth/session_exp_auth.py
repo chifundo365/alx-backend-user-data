bb46fe9ba27a60ac3e55771c4658cee1a43a16c8
= <=>
2024-12-04 12:00:28 +0000

    Expiration?

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -33,7 +33,6 @@ class SessionExpAuth(SessionAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """ retrieves the user id og a session id """
-        print("inside the function")
         if not session_id:
             return None
         user_id = super().user_id_for_session_id(session_id)
@@ -41,7 +40,6 @@ class SessionExpAuth(SessionAuth):
         if not user_id:
             return None
         if type(user_id) == dict:
-            print(f"user_dict: {user_id}")
             if self.session_duration <= 0:
                 return user_id.get('user_id')
             if not user_id.get('created_at'):
@@ -49,8 +47,6 @@ class SessionExpAuth(SessionAuth):
             t_delta = timedelta(seconds=self.session_duration)
             now  = datetime.now()
             if user_id.get('created_at') + t_delta > now:
-                print(f"created_at + duration: {user_id.get('created_at') + t_delta}")
-                print(f"now: {now}")
                 return user_id.get('user_id')
             return None
         return None
